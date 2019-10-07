935d69e2747cef51516e0485dc3d6f0aed63dd87
tahi <gs_tahi@esi.dz>
2019-10-07 15:19:10 +0100

    final Rigide & Solide

--- screens/MailleRigide.js
+++ screens/MailleRigide.js
@@ -1,13 +1,123 @@
 import React, { Component } from "react";
-import {StyleSheet,View, Text} from 'react-native';
+import {StyleSheet,View, Text,TextInput,ScrollView,FlatList} from 'react-native';
+import { Grid } from "native-base";
 
-export default class MailleRigide extends RTCIceCandidate.Component
+
+
+export default class MailleRigide extends React.Component
 {
     render(){
         return(
-                <View> 
-                    <Text> khraaaaaa riggide</Text>
+            
+            <ScrollView horizontal >
+                <ScrollView >
+                  <View style={styles.veiw} > 
+                      <Text style ={styles.do}> Dégradation</Text>
+                       <Text style ={styles.do}> Léger </Text>
+                       <Text style ={styles.do}> Moyen</Text>
+                       <Text style ={styles.do}> Elevé</Text>
+
+                   </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Fissure</Text>
+                    <TextInput placeholder ={'dalle'} style ={styles.do} keyboardType={'numeric'}></TextInput>
+                    <TextInput placeholder ={'dalle'}style ={styles.do} keyboardType={'numeric'}></TextInput>
+                    <TextInput placeholder ={'dalle'}style ={styles.do} keyboardType={'numeric'}></TextInput>
+
                 </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Fracture</Text>
+                    <TextInput style ={styles.do } placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Cassure d'angle</Text>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Epaufrure </Text>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Faiençage/Ecaillage</Text>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'dalle'} keyboardType={'numeric'} ></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Reparation ponctuelle dégradée </Text>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Défaut de joint</Text>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Pompage</Text>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Déclage/Marche</Text>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+              
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Dépot de gomme</Text>
+                    <TextInput   style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'dalle'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                
+                </ScrollView>
+             </ScrollView>
         );
     }
 }
+const styles = StyleSheet.create({
+   veiw :{
+                          height: 70,
+                    flexDirection: "row",
+                    flex: 1,
+                    //borderWidth: 1,
+                    padding: 2,
+                   justifyContent: "flex-start"
+
+
+   },
+   scroll :{
+    padding : 5,
+    },
+    do:  {            
+            width  : 120,
+            alignItems: "center",
+            justifyContent: "center",
+            borderWidth: 1,
+            color :'black',
+        }
+
+
+
+})
--- screens/MailleSolide.js
+++ screens/MailleSolide.js
@@ -1,13 +1,184 @@
 import React, { Component } from "react";
-import {StyleSheet,View, Text} from 'react-native';
+import {StyleSheet,View, Text,TextInput,ScrollView,FlatList} from 'react-native';
 
-export default class Maillesolide extends RTCIceCandidate.Component
+export default class MailleSolide extends React.Component
 {
     render(){
         return(
-                <View> 
-                    <Text> khraaaaaa solide</Text>
+            
+            <ScrollView horizontal >
+                <ScrollView >
+                  <View style={styles.veiw} > 
+                      <Text style ={styles.do}> Dégradation</Text>
+                       <Text style ={styles.do}> Léger </Text>
+                       <Text style ={styles.do}> Moyen</Text>
+                       <Text style ={styles.do}> Elevé</Text>
+
+                   </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Flache</Text>
+                    <TextInput placeholder ={'m'} style ={styles.do} keyboardType={'numeric'}></TextInput>
+                    <TextInput placeholder ={'m'}style ={styles.do} keyboardType={'numeric'}></TextInput>
+                    <TextInput placeholder ={'m'}style ={styles.do} keyboardType={'numeric'}></TextInput>
+
                 </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Orniere frayée</Text>
+                    <TextInput style ={styles.do } placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Fissure de fatigue</Text>
+                    <TextInput style ={styles.do} placeholder ={'m'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'m'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'m'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Faiençage de fatigue </Text>
+                    <TextInput style ={styles.do} placeholder ={'ml'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'ml'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'ml'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style ={styles.do}> Desenrobage/brulure/pelade</Text>
+                    <TextInput style ={styles.do} placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style ={styles.do} placeholder ={'m²'} keyboardType={'numeric'} ></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > fissure de joint</Text>
+                    <TextInput style={styles.do}  placeholder ={'ml'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'ml'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'ml'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Fissure de retrait</Text>
+                    <TextInput style={styles.do}  placeholder ={'ml'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'ml'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'ml'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Faiençage de retrait</Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Déformation en W</Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+              
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Gonfelement-Bourrelet</Text>
+                    <TextInput   style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Terrassement différentiel/marche </Text>
+                    <TextInput style={styles.do}  placeholder ={'m'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Répartition ponctuelle dégradée </Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'}keyboardType={'numeric'} ></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Fissure parabolique</Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Empreinte/poinçennement</Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Nid de poule </Text>
+                    <TextInput style={styles.do}  placeholder ={'U'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Contamination</Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Dépot de gomme</Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Enobré poreux</Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Remontée d'eau </Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Remontée de fine </Text>
+                    <TextInput  style={styles.do} placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    
+
+                </View>
+                <View style={styles.veiw} > 
+                    <Text style={styles.do} > Ressuage </Text>
+                    <TextInput style={styles.do}  placeholder ={'m²'} keyboardType={'numeric'}></TextInput>
+                    
+
+                </View>
+                </ScrollView>
+             </ScrollView>
         );
     }
 }
+const styles = StyleSheet.create({
+   veiw :{
+                          height: 70,
+                    flexDirection: "row",
+                    flex: 1,
+                    //borderWidth: 1,
+                    padding: 2,
+                   justifyContent: "flex-start"
+
+
+   },
+   scroll :{
+    padding : 5,
+    },
+    do:  {            
+            width  : 120,
+            alignItems: "center",
+            justifyContent: "center",
+            borderWidth: 1,
+            color :'black',
+        }
+
+
+
+})
